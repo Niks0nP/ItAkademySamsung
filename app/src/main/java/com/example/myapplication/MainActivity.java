325e72debbd5f7bfbc0ff84afbfe5dc1df49a2c2
Niks0nP <73544093+Niks0nP@users.noreply.github.com>
2020-11-27 00:35:22 +0300

    Home_work

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void OnButtonClick(View v){
+        EditText element1 = (EditText)findViewById(R.id.editTextNumber);
+        EditText element2 = (EditText)findViewById(R.id.editTextNumber2);
+        TextView result = (TextView)findViewById(R.id.text);
+
+        int num1 = Integer.parseInt(element1.getText().toString());
+        int num2 = Integer.parseInt(element2.getText().toString());
+        int resSum = num1 + num2;
+        result.setText(Integer.toString(resSum));
+    }
+    public void OnButtonClick2(View v){
+        EditText element1 = (EditText)findViewById(R.id.editTextNumber);
+        EditText element2 = (EditText)findViewById(R.id.editTextNumber2);
+        TextView result = (TextView)findViewById(R.id.text);
+
+        int num1 = Integer.parseInt(element1.getText().toString());
+        int num2 = Integer.parseInt(element2.getText().toString());
+        int resSum = num1 - num2;
+        result.setText(Integer.toString(resSum));
+    }
+    public void OnButtonClick3(View v){
+        EditText element1 = (EditText)findViewById(R.id.editTextNumber);
+        EditText element2 = (EditText)findViewById(R.id.editTextNumber2);
+        TextView result = (TextView)findViewById(R.id.text);
+
+        int num1 = Integer.parseInt(element1.getText().toString());
+        int num2 = Integer.parseInt(element2.getText().toString());
+        int resSum = num1 * num2;
+        result.setText(Integer.toString(resSum));
+    }
+    public void OnButtonClick4(View v){
+        EditText element1 = (EditText)findViewById(R.id.editTextNumber);
+        EditText element2 = (EditText)findViewById(R.id.editTextNumber2);
+        TextView result = (TextView)findViewById(R.id.text);
+
+        int num1 = Integer.parseInt(element1.getText().toString());
+        int num2 = Integer.parseInt(element2.getText().toString());
+        int resSum = num1 / num2;
+        result.setText(Integer.toString(resSum));
+    }
+}
